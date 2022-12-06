6b78053941b4200b3a72aa4c083abffbe77c3ef4
Alaina Harrison <amvoce1221@gmail.com>
2022-12-05 16:22:09 -0800

    added components folder

--- src/components/Products.js
+++
@@ -1,7 +0,0 @@
-const Products = () => {
-    return <h1>The Products Page</h1>;
-};
-
-export default Products;
-
-//This page will display a list of products
--- src/components/Welcome.js
+++
@@ -1,7 +0,0 @@
-const Welcome = () => {
-    return <h1>The Welcome Page</h1>;
-};
-
-export default Welcome;
-
-//This page will be the welcome/home page
