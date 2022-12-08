e28636f22c9a052e6d78752d85893f972cbaddba
Alaina Harrison <amvoce1221@gmail.com>
2022-12-08 11:05:36 -0800

    added a product details path with product ids

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import MainHeader from './components/MainHeader';
+import ProductDetail from './pages/ProductDetail';
+
 function App() {
   return (
     <div>
@@ -13,6 +15,9 @@ function App() {
         </Route>
         <Route path="/products">
           <Products />
+        </Route>
+        <Route path="/product-detail/:productId">
+          <ProductDetail />
         </Route>
       </main>
     </div>
@@ -27,7 +32,7 @@ export default App;
 * handle different paths for the page
 * load different paths on that page 
 //our-domain.com/welcome => loads Welcome component
-//our-domain.com/products => loads Product compoent
+//our-domain.com/products => loads Product component
 *handle these paths after the domain to then load 
 different components on to the screen
 
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,17 @@
 const Products = () => {
-    return <h1>The Products Page</h1>;
+    return (
+      <section>
+        <h1>The Products Page</h1>
+        <ul>
+            <li>Books</li>
+            <li>Figures</li>
+            <li>Clothing</li>
+        </ul>
+      </section>
+    );
 };
 
 export default Products;
 
 //This page will display a list of products
+//If you click on each product, you will be taken to a page with product info
